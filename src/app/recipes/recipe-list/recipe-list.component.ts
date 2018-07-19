b28be16ee120092b3f392bb2d5462c680531749f
Samik Basu <39623@Hexaware.com>
2018-07-19 19:32:27 +0530

    Changes Added - Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSeleted = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('test recipe1', 'test1', 'http://survival-mastery.com/wp-content/uploads/2015/03/Camping-Food-Idea-810x518.jpg'),
+    new Recipe('test recipe2', 'test2', 'http://survival-mastery.com/wp-content/uploads/2015/03/Camping-Food-Idea-810x518.jpg')
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSeleted.emit(recipe);
   }
 
 }
